b386e1bfcd481087a16dcc4bd68f84188bb1391f
rakibul-wdp <rakibul.wdp@gmail.com>
2023-01-26 11:08:15 +0600

    setup root routes wtih basic structure

--- server/index.js
+++ server/index.js
@@ -4,6 +4,10 @@ import dotenv from 'dotenv';
 import express from 'express';
 import helmet from 'helmet';
 import morgan from 'morgan';
+import clientRoutes from "./routes/client.js";
+import generalRoutes from "./routes/general.js";
+import managementRoutes from "./routes/management.js";
+import salesRoutes from "./routes/sales.js";
 
 /* CONFIGURATION */
 dotenv.config();
