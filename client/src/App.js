1a3f47e9166aa560d68534ff3a8e7c6adf6e8fdc
rakibul-wdp <rakibul.wdp@gmail.com>
2023-01-27 05:21:08 +0600

    create products page header component

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import { useSelector } from "react-redux";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Dashboard from "scenes/dashboard";
 import Layout from "scenes/layout";
+import Products from "scenes/products";
 import { themeSettings } from "theme";
 
 function App() {
@@ -20,6 +21,7 @@ function App() {
             <Route element={<Layout />}>
               <Route path="/" element={<Navigate to="/dashboard" replace />} />
               <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/products" element={<Products />} />
             </Route>
           </Routes>
         </ThemeProvider>
