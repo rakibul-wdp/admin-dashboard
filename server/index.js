34dd600a912f7cdaad578516a10b0384890bdfab
rakibul-wdp <rakibul.wdp@gmail.com>
2023-01-26 10:42:10 +0600

    first commit with basic server structure

---
+++ server/index.js
@@ -0,0 +1,17 @@
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import express from 'express';
+import helmet from 'helmet';
+import mongoose from 'mongoose';
+import morgan from 'morgan';
+
+/* CONFIGURATION */
+
+express
+bodyParser
+mongoose
+cors
+dotenv
+helmet
+morgan
