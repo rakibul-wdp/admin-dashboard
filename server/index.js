328615075732715d9e3560c4df982988289a9c53
rakibul-wdp <rakibul.wdp@gmail.com>
2023-01-26 10:49:13 +0600

    setup configuration

--- server/index.js
+++ server/index.js
@@ -3,15 +3,15 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
 import helmet from 'helmet';
-import mongoose from 'mongoose';
 import morgan from 'morgan';
 
 /* CONFIGURATION */
-
-express
-bodyParser
-mongoose
-cors
-dotenv
-helmet
-morgan
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin"}));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cors());
