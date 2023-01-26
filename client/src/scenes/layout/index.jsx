3b44692edbcc06848725da54aacb1b756456ad90
rakibul-wdp <rakibul.wdp@gmail.com>
2023-01-26 15:28:04 +0600

    create navbar component

--- client/src/scenes/layout/index.jsx
+++ client/src/scenes/layout/index.jsx
@@ -1,5 +1,16 @@
+import { Box } from "@mui/material";
+import Navbar from "components/Navbar";
+import { Outlet } from "react-router-dom";
+
 const Layout = () => {
-  return <div>Layout</div>;
+  return (
+    <Box width="100%" height="100%">
+      <Box>
+        <Navbar />
+        <Outlet />
+      </Box>
+    </Box>
+  );
 };
 
 export default Layout;
