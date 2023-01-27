01754cebbcd1e672fb8a2df9442970ff2058475d
rakibul-wdp <rakibul.wdp@gmail.com>
2023-01-27 15:12:23 +0600

    create customers page

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { createTheme } from "@mui/material/styles";
 import { useMemo } from "react";
 import { useSelector } from "react-redux";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Customers from "scenes/customers";
 import Dashboard from "scenes/dashboard";
 import Layout from "scenes/layout";
 import Products from "scenes/products";
@@ -22,6 +23,7 @@ function App() {
               <Route path="/" element={<Navigate to="/dashboard" replace />} />
               <Route path="/dashboard" element={<Dashboard />} />
               <Route path="/products" element={<Products />} />
+              <Route path="/customers" element={<Customers />} />
             </Route>
           </Routes>
         </ThemeProvider>
