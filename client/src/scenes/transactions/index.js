1b6b60f1e191aa25bde396abc0a551fb37ec595e
rakibul-wdp <rakibul.wdp@gmail.com>
2023-01-27 17:10:33 +0600

    create data grid custom toolbar

--- client/src/scenes/transactions/index.js
+++ client/src/scenes/transactions/index.js
@@ -14,6 +14,7 @@ const Transactions = () => {
   const [sort, setSort] = useState({});
   const [search, setSearch] = useState("");
 
+  const [searchInput, setSearchInput] = useState("");
   const { data, isLoading } = useGetTransactionsQuery({
     page,
     pageSize,
