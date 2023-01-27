17ca93f267e54246758a3844df796875cbcad6cd
rakibul-wdp <rakibul.wdp@gmail.com>
2023-01-27 17:43:20 +0600

    create ge geograpy data from user data to create map

--- server/routes/client.js
+++ server/routes/client.js
@@ -1,10 +1,14 @@
 import express from "express";
-import { getCustomers, getProducts, getTransactions } from "../controllers/client.js";
+import {
+  getCustomers, getGeography, getProducts,
+  getTransactions
+} from "../controllers/client.js";
 
 const router = express.Router();
 
 router.get("/products", getProducts);
 router.get("/customers", getCustomers);
 router.get("/transactions", getTransactions);
+router.get("/geography", getGeography);
 
 export default router;
