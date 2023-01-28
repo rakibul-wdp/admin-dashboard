7cbdfa844b0530afa0d567d2eb4c13a6e5f5ca4b
rakibul-wdp <rakibul.wdp@gmail.com>
2023-01-28 11:35:48 +0600

    create admin page ui without custom column menu

--- client/src/scenes/admin/index.jsx
+++ client/src/scenes/admin/index.jsx
@@ -1,4 +1,7 @@
-import { useTheme } from "@mui/material";
+import { Box, useTheme } from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
+import CustomColumnMenu from "components/DataGridCustomColumnMenu";
+import Header from "components/Header";
 import { useGetAdminsQuery } from "state/api";
 
 const Admin = () => {
@@ -46,7 +49,49 @@ const Admin = () => {
     },
   ];
 
-  return <div>Admin</div>;
+  return (
+    <Box m="1.5rem 2.5rem">
+      <Header title="ADMINS" subtitle="Managing admins and list of admins" />
+      <Box
+        mt="40px"
+        height="75vh"
+        sx={{
+          "& .MuiDataGrid-root": {
+            border: "none",
+          },
+          "& .MuiDataGrid-cell": {
+            borderBottom: "none",
+          },
+          "& .MuiDataGrid-columnHeaders": {
+            backgroundColor: theme.palette.background.alt,
+            color: theme.palette.secondary[100],
+            borderBottom: "none",
+          },
+          "& .MuiDataGrid-virtualScroller": {
+            backgroundColor: theme.palette.primary.light,
+          },
+          "& .MuiDataGrid-footerContainer": {
+            backgroundColor: theme.palette.background.alt,
+            color: theme.palette.secondary[100],
+            borderTop: "none",
+          },
+          "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
+            color: `${theme.palette.secondary[200]} !important`,
+          },
+        }}
+      >
+        <DataGrid
+          loading={isLoading || !data}
+          getRowId={(row) => row._id}
+          rows={data || []}
+          columns={columns}
+          components={{
+            ColumnMenu: CustomColumnMenu,
+          }}
+        />
+      </Box>
+    </Box>
+  );
 };
 
 export default Admin;
