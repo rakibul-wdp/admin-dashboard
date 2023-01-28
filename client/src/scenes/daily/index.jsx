73f745953ba02ac80c0cbdaeb8d5d1b2daf1b9f5
rakibul-wdp <rakibul.wdp@gmail.com>
2023-01-28 09:57:01 +0600

    create daily chart functionality

---
+++ client/src/scenes/daily/index.jsx
@@ -0,0 +1,50 @@
+import { useTheme } from "@mui/material";
+import { useMemo, useState } from "react";
+import "react-datepicker/dist/react-datepicker.css";
+import { useGetSalesQuery } from "state/api";
+
+const Daily = () => {
+  const [startDate, setStartDate] = useState(new Date("2021-02-01"));
+  const [endDate, setEndDate] = useState(new Date("2021-03-01"));
+  const { data } = useGetSalesQuery();
+  const theme = useTheme();
+
+  const [formattedData] = useMemo(() => {
+    if (!data) return [];
+
+    const { dailyData } = data;
+    const totalSalesLine = {
+      id: "totalSales",
+      color: theme.palette.secondary.main,
+      data: [],
+    };
+    const totalUnitsLine = {
+      id: "totalUnits",
+      color: theme.palette.secondary[600],
+      data: [],
+    };
+
+    Object.values(dailyData).forEach(({ date, totalSales, totalUnits }) => {
+      const dateFormatted = new Date(date);
+      if (dateFormatted >= startDate && dateFormatted <= endDate) {
+        const splitDate = date.substring(date.indexOf("-") + 1);
+
+        totalSalesLine.data = [
+          ...totalSalesLine.data,
+          { x: splitDate, y: totalSales },
+        ];
+        totalUnitsLine.data = [
+          ...totalUnitsLine.data,
+          { x: splitDate, y: totalUnits },
+        ];
+      }
+    });
+
+    const formattedData = [totalSalesLine, totalUnitsLine];
+    return [formattedData];
+  }, [data, startDate, endDate]); // eslint-disable-line react-hooks/exhaustive-deps
+
+  return <div>Daily</div>;
+};
+
+export default Daily;
